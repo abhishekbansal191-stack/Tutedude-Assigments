58f6e8465cee21223d46283c824eef6d9bb87056
abhishekbansal191-stack <abhishekbansal191@gmail.com>
2025-09-11 20:53:41 +0530

    Add files via upload

---
+++ Assignment-1(Task-1).py
@@ -0,0 +1,10 @@
+#Assignment 1
+#Task 1: Perform Basic Mathematical Operations
+f=input("Enter the First number :")
+s=input("Enter the Second number :")
+f=float(f)
+s=float(s)
+print("Addition:", f+s)
+print("Multiplication:", f*s)
+print("Subtraction:", f-s)
+print("Division:", f/s)
---
+++ Assignment-1(Task-2).py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+f= input("Enter the First name :")
+s=input("Enter the Second name :")
+f=str(f)
+s=str(s)
+print("Hello,", f+" "+s, "! Welcome to the python programme")
