7a1c8aefedeb323970ca476952879568b1ca8afa
abhishekbansal191-stack <abhishekbansal191@gmail.com>
2025-09-27 08:19:27 +0530

    Add files via upload

---
+++ Assignment3(Task2).py
@@ -0,0 +1,8 @@
+#Task 2: Using the Math Module for Calculations
+n=int(input("Enter a number:"))
+from math import sqrt
+print("Square root:", sqrt(n))
+from math import log
+print("Logarithm:", log(n))
+from math import sin
+print("Sine:", sin(n))
