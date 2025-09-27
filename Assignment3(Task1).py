c5cf3785580f2b2d114c8d99cc07007114d1fc09
abhishekbansal191-stack <abhishekbansal191@gmail.com>
2025-09-27 08:12:08 +0530

    Add files via upload

---
+++ Assignment3(Task1).py
@@ -0,0 +1,8 @@
+#Task 1: Calculate Factorial Using a Function
+n= int(input("Enter a number:"))
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n-1)
+print("Factorial of", n, "is:", factorial(n))
